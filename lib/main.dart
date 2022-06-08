578c479343a0afa6a2371053b3ee3de26d9cded2
Avinash Uchadiya <avinashuchadiya786@gmail.com>
2022-06-08 16:56:05 +0530

    created nested container to create a layout for pole app

--- lib/main.dart
+++ lib/main.dart
@@ -21,50 +21,175 @@ class Avinash extends StatelessWidget {
       appBar: AppBar(
           backgroundColor: Colors.green.shade200,
           title: const Text('Pole App')),
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 2),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 15,
           ),
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.all(30),
-          child: Column(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                height: 100,
-                width: 100,
-                margin: const EdgeInsets.all(2),
-                padding: const EdgeInsets.all(2),
-                color: Colors.green[300],
-                child: const Text('Option 1'),
+              Card(
+                margin: EdgeInsets.all(5),
+                elevation: 0,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'USER_ID',
+                    style: TextStyle(color: Colors.green[400]),
+                  ),
+                ),
               ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: const EdgeInsets.all(2),
-                padding: const EdgeInsets.all(2),
-                color: Colors.green[300],
-                child: const Text('Option 2'),
+              SizedBox(
+                width: 2,
               ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: const EdgeInsets.all(2),
-                padding: const EdgeInsets.all(2),
-                color: Colors.green[300],
-                child: const Text('Option 3'),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: const EdgeInsets.all(2),
-                padding: const EdgeInsets.all(2),
-                color: Colors.green[300],
-                child: const Text('Option 4'),
+              Card(
+                margin: EdgeInsets.all(5),
+                elevation: 0,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "POST_COUNT",
+                    style: TextStyle(color: Colors.green[400]),
+                  ),
+                ),
               ),
             ],
-          )),
+          ),
+          Container(
+            padding: EdgeInsets.all(25),
+            child: Text(
+              'QUESTION WILL BE HERE.....',
+              style: TextStyle(color: Colors.green),
+            ),
+          ),
+
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Card(
+                    margin: EdgeInsets.all(15),
+                    elevation: 5,
+                    child: Container(
+                      padding: EdgeInsets.all(40),
+                      child: Text(
+                        'OPTION 1',
+                        style: TextStyle(color: Colors.green[400]),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(15),
+                    elevation: 5,
+                    child: Container(
+                      padding: EdgeInsets.all(40),
+                      child: Text(
+                        "OPTION 2",
+                        style: TextStyle(color: Colors.green[400]),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Card(
+                    margin: EdgeInsets.all(15),
+                    elevation: 5,
+                    child: Container(
+                      padding: EdgeInsets.all(40),
+                      child: Text(
+                        'OPTION 3',
+                        style: TextStyle(color: Colors.green[400]),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(15),
+                    elevation: 5,
+                    child: Container(
+                      padding: EdgeInsets.all(40),
+                      child: Text(
+                        "OPTION 4  ",
+                        style: TextStyle(color: Colors.green[400]),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+          // Container(
+          //   // height: double.infinity,
+          //   // width: double.infinity,
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.black, width: 2),
+          //   ),
+          //   margin: const EdgeInsets.all(20),
+          //   padding: const EdgeInsets.all(30),
+          //   child: Row(
+          //     children: [],
+          //   ),
+          // )
+        ],
+      ),
+
+      // body: Container(
+      //     height: double.infinity,
+      //     width: double.infinity,
+      //     decoration: BoxDecoration(
+      //       border: Border.all(color: Colors.black, width: 2),
+      //     ),
+      //     margin: const EdgeInsets.all(20),
+      //     padding: const EdgeInsets.all(30),
+      //     child: Column(
+      //       children: [
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           margin: const EdgeInsets.all(2),
+      //           padding: const EdgeInsets.all(2),
+      //           color: Colors.green[300],
+      //           child: const Text('Option 1'),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           margin: const EdgeInsets.all(2),
+      //           padding: const EdgeInsets.all(2),
+      //           color: Colors.green[300],
+      //           child: const Text('Option 2'),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           margin: const EdgeInsets.all(2),
+      //           padding: const EdgeInsets.all(2),
+      //           color: Colors.green[300],
+      //           child: const Text('Option 3'),
+      //         ),
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           margin: const EdgeInsets.all(2),
+      //           padding: const EdgeInsets.all(2),
+      //           color: Colors.green[300],
+      //           child: const Text('Option 4'),
+      //         ),
+      //       ],
+      //     )),
     );
   }
 }
